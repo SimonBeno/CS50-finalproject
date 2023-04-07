8d73127fbd2e899a8247aeefa838ef2843c78206
Simon Beno <129320318+SimonBeno@users.noreply.github.com>
2023-04-07 17:23:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 import re
 
-#from cs50 import SQL
+#-> from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, url_for
-# from flask_session import Session
+#-> from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask import jsonify
 
@@ -19,7 +19,8 @@ app = Flask(__name__)
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+#-> Session(app)
+session(app)
 
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///habits.db")
